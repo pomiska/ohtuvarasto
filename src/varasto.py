0c3a8d1e50bc7af373c8866048ec2fdca9326a93
pomiska <miska99@live.fi>
2022-11-14 15:50:28 +0200

    github actions collaborating with pylint finally

--- src/varasto.py
+++ src/varasto.py
@@ -1,12 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo=0):
 
-        #self.tilavuus = max(0, tilavuus) testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = max(0, tilavuus)
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
